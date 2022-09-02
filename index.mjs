4088e0a82f570e09ab0210c675fcc9f9081b5c99
Joshua Bloom <bloomblockchain@gmail.com>
2022-09-02 16:57:01 -0400

    finished frontend

--- index.mjs
+++ index.mjs
@@ -14,9 +14,9 @@ const getBalance = async (who) =>
       // callback function receives a participant and returns the formatted currency balance
       stdlib.formatCurrency(await stdlib.balanceOf(who), 4);
 // balance of deployer before any tranactions      
-const beforeDeployer = await getBalance(Deployer);
+const beforeDeployer = await getBalance(accDeployer);
 // balance of receiver before any transactions
-const beforeReceiver = await getBalance(Receiver);
+const beforeReceiver = await getBalance(accReceiver);
 
 // deployer deploys the contract
 const ctcDeployer = accDeployer.contract(backend);
@@ -27,4 +27,28 @@ const ctcReceiver = accReceiver.contract(backend, ctcDeployer.getInfo());
 const thePassword = stdlib.randomUInt();
 
 // initializing the backend for both participants
+await Promise.all([
+  // initializing backend for deployer
+  backend.Deployer(ctcDeployer, {
+    amount: stdlib.parseCurrency(50),
+    password: thePassword,
+  }),
+  // initializing the backend for receiver
+  backend.Receiver(ctcReceiver, {
+    obtainPassword: () => {
+      console.log('Deployer asked to give the preimage.');
+      console.log(`Returning: ${thePassword}`);
+      return thePassword;
+    },
+  }),
+]);
 
+// balance for deployer after the smart contract executes
+const afterDeployer = await getBalance(accDeployer);
+// balance for the receiver after the smart contract executes
+const afterReceiver = await getBalance(accReceiver);
+
+// logging the balances for the deployer
+console.log(`Deployer went from ${beforeDeployer} to ${afterDeployer}.`);
+// logging the balances for the receiver
+console.log(`Receiver went from ${beforeReceiver} to ${afterReceiver}`);
