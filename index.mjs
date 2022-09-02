dd3c237f8ecc8a6d5d29625f83b860983557e77a
Joshua Bloom <bloomblockchain@gmail.com>
2022-09-02 11:40:21 -0400

    updating front end participant interface

--- index.mjs
+++ index.mjs
@@ -2,26 +2,30 @@ import {loadStdlib} from '@reach-sh/stdlib';
 import * as backend from './build/index.main.mjs';
 const stdlib = loadStdlib(process.env);
 
-const startingBalance = stdlib.parseCurrency(100);
+// sets the amount value
+const amount = stdlib.parseCurrency(100);
 
-const [ accAlice, accBob ] =
-  await stdlib.newTestAccounts(2, startingBalance);
-console.log('Hello, Alice and Bob!');
+// accounts are created 
+const [ accDeployer, accReceiver ] =
+  await stdlib.newTestAccounts(2, amount);
+console.log('Hello, Deployer and Receiver!');
 
+// deployer launches the contract and then receiver attaches to the contract and gets information about deployer
 console.log('Launching...');
-const ctcAlice = accAlice.contract(backend);
-const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
+const ctcDeployer = accDeployer.contract(backend);
+const ctcReceiver = accReceiver.contract(backend, accDeployer.getInfo());
 
+// starts the backend for deployer and receiver
 console.log('Starting backends...');
 await Promise.all([
-  backend.Alice(ctcAlice, {
+  backend.Deployer(ctcDeployer, {
     ...stdlib.hasRandom,
-    // implement Alice's interact object here
+    // implement deployers's interact object here
   }),
-  backend.Bob(ctcBob, {
+  backend.Receiver(ctcReceiver, {
     ...stdlib.hasRandom,
-    // implement Bob's interact object here
+    // implement receiver's interact object here
   }),
 ]);
 
-console.log('Goodbye, Alice and Bob!');
+console.log('Funds have been released!');
