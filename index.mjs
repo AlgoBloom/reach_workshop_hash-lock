eb6b43e81c346f0d724bf2769a496c90208bc548
Joshua Bloom <bloomblockchain@gmail.com>
2022-09-02 16:43:25 -0400

    Deploying the contract from the front end and initializing the backends.

--- index.mjs
+++ index.mjs
@@ -9,3 +9,22 @@ const startingBalance = stdlib.parseCurrency(100);
 const accDeployer = await stdlib.newTestAccount(startingBalance);
 const accReceiver = await stdlib.newTestAccount(startingBalance);
 
+// create a function to get the balance of a participant and then shows us the before balances
+const getBalance = async (who) =>
+      // callback function receives a participant and returns the formatted currency balance
+      stdlib.formatCurrency(await stdlib.balanceOf(who), 4);
+// balance of deployer before any tranactions      
+const beforeDeployer = await getBalance(Deployer);
+// balance of receiver before any transactions
+const beforeReceiver = await getBalance(Receiver);
+
+// deployer deploys the contract
+const ctcDeployer = accDeployer.contract(backend);
+// receiver attaches to the to contract that the deployer deployed
+const ctcReceiver = accReceiver.contract(backend, ctcDeployer.getInfo());
+
+// creating the password
+const thePassword = stdlib.randomUInt();
+
+// initializing the backend for both participants
+
